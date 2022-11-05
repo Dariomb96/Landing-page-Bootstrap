e2c09d3d4d8f6064a529418f9631f7eedc2b36ee
Dariomb96 <betancourtdariom@gmail.com>
2022-11-05 18:47:31 -0300

    pajuste de variable total a local ya que con la simplificacion de codigo era innecesario definirla globalmente

--- tickets.js
+++ tickets.js
@@ -3,11 +3,10 @@ const opciones = document.getElementById('opcionesDescuento');
 const tickets = document.getElementById('cTickets')
 const resumen = document.getElementById('btnResumen');
 const valorTicket = 200;
-let total = 0;
 
 function calcularTotal() {
     let cantidadTickets = tickets.value
-    total = cantidadTickets * valorTicket;
+    let total = cantidadTickets * valorTicket;
     let nuevoTotal = total * opciones.value;
     totalTicket.innerText = `Total a pagar: $${nuevoTotal}`;
 }
